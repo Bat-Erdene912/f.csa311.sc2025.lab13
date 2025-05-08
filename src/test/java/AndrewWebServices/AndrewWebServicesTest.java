6fce3862c05c093bd8bb56bf004722126faadd46
Bat-Erdene912 <erdenee0912@gmail.com>
2025-05-08 15:56:22 +0800

    Update AndrewWebServicesTest.java

--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -2,47 +2,54 @@ package AndrewWebServices;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class AndrewWebServicesTest {
-    Database database;
-    RecSys recommender;
-    PromoService promoService;
-    AndrewWebServices andrewWebService;
+    InMemoryDatabase fakeDatabase;
+    RecSysStub stubRecommender;
+    PromoService mockPromoService;
+    AndrewWebServices aws;
 
     @Before
     public void setUp() {
-        // You need to use some mock objects here
-        database = new Database(); // We probably don't want to access our real database...
-        recommender = new RecSys();
-        promoService = new PromoService();
+        // Fake
+        fakeDatabase = new InMemoryDatabase();
+        fakeDatabase.addUser("Scotty", 17214);
 
-        andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        // Stub
+        stubRecommender = new RecSysStub();
+
+        // Mock
+        mockPromoService = mock(PromoService.class);
+
+        aws = new AndrewWebServices(fakeDatabase, stubRecommender, mockPromoService);
     }
 
     @Test
     public void testLogIn() {
-        // This is taking way too long to test
-        assertTrue(andrewWebService.logIn("Scotty", 17214));
+        assertTrue(aws.logIn("Scotty", 17214));
     }
 
     @Test
     public void testGetRecommendation() {
-        // This is taking way too long to test
-        assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
+        assertEquals("Animal House", aws.getRecommendation("Scotty"));
     }
 
     @Test
     public void testSendEmail() {
-        // How should we test sendEmail() when it doesn't have a return value?
-        // Hint: is there something from Mockito that seems useful here?
+        aws.sendPromoEmail("test@example.com");
+        verify(mockPromoService, times(1)).mailTo("test@example.com");
     }
 
     @Test
     public void testNoSendEmail() {
-        // How should we test that no email has been sent in certain situations (like right after logging in)?
-        // Hint: is there something from Mockito that seems useful here?
+        
+        verify(mockPromoService, never()).mailTo(anyString());
     }
 }
