13babdaae7b9c059eb76bf68d31f9a4b95941c57
Bat-Erdene912 <erdenee0912@gmail.com>
2025-05-08 15:55:44 +0800

    Update RecSys.java

--- src/main/java/AndrewWebServices/RecSys.java
+++ src/main/java/AndrewWebServices/RecSys.java
@@ -1,19 +1,11 @@
 package AndrewWebServices;
 
-import java.util.concurrent.TimeUnit;
-
-/*
- * RecSys is an implementation of the AndrewWS recommendation system which uses super advanced machine learning to given movie recommendations. 
- * 
- * DO NOT MODIFY this class.
- */
-public class RecSys {
+public class RecSysStub extends RecSys {
+    @Override
     public String getRecommendation(String accountName) {
-        try {
-            TimeUnit.SECONDS.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if ("Scotty".equals(accountName)) {
+            return "Animal House";
         }
-        return "Animal House";
+        return "Unknown";
     }
 }
